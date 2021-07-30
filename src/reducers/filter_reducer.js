d3efcea28910c59018a225d54185adfe1f4b60b2
niklavs <niklavs.p@gmail.com>
2021-07-30 19:00:09 +0300

    products page sorting functionality

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -33,7 +33,29 @@ const filter_reducer = (state, action) => {
     case UPDATE_SORT:
       return {
         ...state,
-        [action.payload.name]: [action.payload.value],
+        [action.payload.name]: action.payload.value,
+      };
+
+    case SORT_PRODUCTS:
+      return {
+        ...state,
+        filtered_products: state.filtered_products.sort((a, b) => {
+          switch (state.sort) {
+            case 'price-lowest':
+              return a.price - b.price;
+
+            case 'price-highest':
+              return b.price - a.price;
+
+            case 'name-a':
+              // return a.name > b.name ? 1 : b.name > a.name ? -1 : 0;
+              return a.name.localeCompare(b.name);
+
+            case 'name-z':
+              // return a.name < b.name ? 1 : b.name < a.name ? -1 : 0;
+              return b.name.localeCompare(a.name);
+          }
+        }),
       };
 
     default:
